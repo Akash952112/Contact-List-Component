4873b85770cd159d587783c5daea45b181e99df6
Akash-Sondarva <akashsondarva21@gmail.com>
2025-08-20 16:27:19 +0530

    Created contact search

---
+++ src/components/ContactList.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./ContactList.css";
+// used own static data as no json was provided
+import CONTACT_DATA from "../constant";
+
+const ContactList = () => {
+  const [search, setSearch] = useState("");
+  const [contactList, setContactList] = useState(CONTACT_DATA);
+
+  const handleSearch = (searchTerm: string) => {
+    setSearch(searchTerm);
+    if (searchTerm == "") {
+      setContactList(CONTACT_DATA);
+      return;
+    }
+    const updatedList = CONTACT_DATA.filter((contact) => {
+      return contact.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setContactList(updatedList);
+  };
+
+  return (
+    <div>
+      <h1>My Contacts</h1>
+      <input
+        name="searchBar"
+        value={search}
+        onChange={(e) => handleSearch(e.target.value)}
+        className="input"
+        placeholder="Search"
+      />
+      <div className="container">
+        {contactList.map((data) => (
+          <div className="contact">
+            <img src={data.avatar} className="contactAvatar" />
+            <div className="contactDetail">
+              <div className="contactName">
+                {data.name.split("").map((char) => (
+                  <span
+                    className={
+                      search.toLowerCase().includes(char.toLowerCase())
+                        ? "heighLightText"
+                        : ""
+                    }
+                  >
+                    {char}
+                  </span>
+                ))}
+              </div>
+              <div>{data.number}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ContactList;
---
+++ src/constant.ts
@@ -0,0 +1,36 @@
+const CONTACT_DATA = [
+  {
+    name: "Akash sondarva",
+    number: "123456789",
+    avatar: "https://robohash.org/officiaautsed.png?size=50x50&amp;set=set1",
+  },
+  {
+    name: "Yash Bhandari",
+    number: "1232136789",
+    avatar: "https://robohash.org/quodtemporesint.png?size=50x50&amp;set=set1",
+  },
+  {
+    name: "Khushi Desai",
+    number: "123123123354",
+    avatar:
+      "https://robohash.org/praesentiumnonenim.png?size=50x50&amp;set=set1",
+  },
+  {
+    name: "Manali Jain",
+    number: "9789870870",
+    avatar: "https://robohash.org/officiaautsed.png?size=50x50&amp;set=set1",
+  },
+  {
+    name: "Nikunj Patel",
+    number: "345869797",
+    avatar:
+      "https://robohash.org/quaeratpossimusblanditiis.png?size=50x50&set=set1",
+  },
+  {
+    name: "Dhruv Khetiya",
+    number: "42576587698",
+    avatar: "https://robohash.org/isterepellatodit.png?size=50x50&set=set1",
+  },
+];
+
+export default CONTACT_DATA;
